741e27279427ecaee73ee33304964a4be1f29cd9
lsf1111 <lsf111122>
2017-07-30 15:46:35 +0400

    Adding Floating Button

--- app/src/main/java/am/ysu/ysu/HomeActivity.java
+++ app/src/main/java/am/ysu/ysu/HomeActivity.java
@@ -1,6 +1,7 @@
 package am.ysu.ysu;
 
-import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,8 +18,18 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        button_dasacucak = (Button)findViewById(R.id.button_dasacucak);
+        button_dasacucak = (Button)findViewById(R.id.button7);
         button_dasacucak.setOnClickListener(this);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
     }
 
